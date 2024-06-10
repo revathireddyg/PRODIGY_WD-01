d5c91524dcb7c5e1d092cbf99d7b201993f7beaa
revathireddyg <142475536+revathireddyg@users.noreply.github.com>
2024-06-10 18:07:24 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,16 @@
+// function changeBg(bg,title) {
+//   const banner=document.querySelector('.banner');
+//   const contents=document.querySelectorAll('.content');
+//   banner.Style.background ='url("../img/movies/${bg}")';
+//   banner.style.backgroundsize = 'cover';
+//   banner.style.backgroundPosition = 'center';
+
+// contents.forEach(content => {
+//   content.classList.remove('active');
+//     if(content.classList.contains(title)) {
+//       content.classList.add('active');
+//     }
+  
+// });
+
+// }
